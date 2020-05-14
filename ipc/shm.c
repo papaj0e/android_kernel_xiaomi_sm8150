851ebdabc1bdde760c94fa6b1c8e8b621a044e91
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-05-14 13:55:09 +0530

    sm8150: squashed commit for code-quality fixes using gcc-9.3.0 with -O3
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    (cherry picked from commit f4a958991f81a5a0b2f3774822e7641868ed03c5)

--- ipc/shm.c
+++ ipc/shm.c
@@ -1552,7 +1552,7 @@ out:
 
 SYSCALL_DEFINE3(shmat, int, shmid, char __user *, shmaddr, int, shmflg)
 {
-	unsigned long ret;
+	unsigned long ret = 0;
 	long err;
 
 	err = do_shmat(shmid, shmaddr, shmflg, &ret, SHMLBA);
@@ -1570,7 +1570,7 @@ SYSCALL_DEFINE3(shmat, int, shmid, char __user *, shmaddr, int, shmflg)
 
 COMPAT_SYSCALL_DEFINE3(shmat, int, shmid, compat_uptr_t, shmaddr, int, shmflg)
 {
-	unsigned long ret;
+	unsigned long ret = 0;
 	long err;
 
 	err = do_shmat(shmid, compat_ptr(shmaddr), shmflg, &ret, COMPAT_SHMLBA);
