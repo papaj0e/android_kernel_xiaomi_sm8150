64af4d8ef726b956f254c394170e876bfd560ec7
Peter Ivanov <pivanov@hotmail.com>
2023-06-21 15:07:05 +0300

    fs/exec: use str_has_prefix to make code cleaner

--- fs/exec.c
+++ fs/exec.c
@@ -1854,24 +1854,15 @@ static int do_execveat_common(int fd, struct filename *filename,
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_sig = current->signal;
-		else if (unlikely(!strncmp(filename->name,
-					   HWCOMPOSER_BIN_PREFIX,
-					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
-		} else if (unlikely(!strncmp(filename->name,
-					   UDFPS_BIN_PREFIX,
-					   strlen(UDFPS_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
-		} else if (unlikely(!strncmp(filename->name,
-					   SFLINGER_BIN_PREFIX,
-					   strlen(SFLINGER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_AFFINE;
-			set_cpus_allowed_ptr(current, cpu_prime_mask);
-		} else if (unlikely(!strncmp(filename->name,
-					   QTIHW_BIN_PREFIX,
-					   strlen(QTIHW_BIN_PREFIX)))) {
+		else if (unlikely(
+			str_has_prefix(filename->name,
+					  		HWCOMPOSER_BIN_PREFIX) ||
+			str_has_prefix(filename->name,
+					   		UDFPS_BIN_PREFIX)      ||
+			str_has_prefix(filename->name,
+					   		SFLINGER_BIN_PREFIX)   ||
+			str_has_prefix(filename->name,
+					   		QTIHW_BIN_PREFIX))) {
 			current->pc_flags |= PC_PRIME_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_prime_mask);
 		}
