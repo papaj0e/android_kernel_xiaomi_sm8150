ccc4e9ad0285b3aa14fcee8d8d7850f185b6753b
Peter Ivanov <pivanov@hotmail.com>
2023-09-04 11:07:12 +0300

    Revert "treewide: affine few critical processes to prime cluster"
    This reverts commit b6f2db45e91b50cfc064d8902780d12765930529.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4964,7 +4964,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
-	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -5033,11 +5032,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
-	if (!strcmp(device->name, "kgsl_3d0_irq"))
-		irqflags |= IRQF_PRIME_AFFINE;
-
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler, irqflags,
+				  kgsl_irq_handler, IRQF_TRIGGER_HIGH,
 				  device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
@@ -5191,7 +5187,7 @@ static long kgsl_run_one_worker_perf(struct kthread_worker *worker,
 		struct task_struct **thread, const char *name)
 {
 	kthread_init_worker(worker);
-	*thread = kthread_run_perf_critical(cpu_prime_mask,
+	*thread = kthread_run_perf_critical(cpu_perf_mask,
 		kthread_worker_fn, worker, name);
 	if (IS_ERR(*thread)) {
 		pr_err("unable to start %s\n", name);
